073b4b9af1ae76d5493a23a54ccfc7cf9d40d2db
User <huseynovali612@gmail.com>
2023-06-08 21:59:59 +0400

    new

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -2,20 +2,24 @@
 module.exports = {
   content: [
     "./src/**/*.{js,jsx,ts,tsx}",
+
   ],
 
   theme: {
-    colors: {
-      dark_Blue: "#03045E",
-      rich_Blue: "#023E8A",
-      cerulean_Blue: "#0077B6",
-      vivid_Cerulean: "#0096C7",
-      vivid_Cyan: "#00B4D8",
-      medium_Turquoise: "#48CAE4",
-      baby_Blue: "#90E0EF",
-      light_Blue: "#ADE8F4",
-      pale_Cyan: "#CAF0F8"
-    }
+    extend: {
+      colors: {
+        "dark_Blue": "#03045E",
+        "rich_Blue": "#023E8A",
+        "cerulean_Blue": "#0077B6",
+        "vivid_Cerulean": "#0096C7",
+        "vivid_Cyan": "#00B4D8",
+        "medium_Turquoise": "#48CAE4",
+        "baby_Blue": "#90E0EF",
+        "light_Blue": "#ADE8F4",
+        "pale_Cyan": "#CAF0F8"
+      }
+    },
+
   },
   plugins: [],
 }

4fec44b2f2562070b2da94056e05274dc85650a9
User <huseynovali612@gmail.com>
2023-06-09 14:09:04 +0400

    Change allevents

--- client/src/page/AllEvents/AllEvents.jsx
+++ client/src/page/AllEvents/AllEvents.jsx
@@ -4,14 +4,15 @@ import {
   FaClock,
   FaMapMarkerAlt,
   FaHeart,
-  FaShare,} from "react-icons/fa";
-  
-  import DatePicker from 'react-datepicker';
-  import 'react-datepicker/dist/react-datepicker.css';
-  
-  import InputRange from 'react-input-range';
-  import 'react-input-range/lib/css/index.css';
-  
+  FaShare,
+} from "react-icons/fa";
+
+import DatePicker from 'react-datepicker';
+import 'react-datepicker/dist/react-datepicker.css';
+
+import InputRange from 'react-input-range';
+import 'react-input-range/lib/css/index.css';
+
 import "./AllEvents.css";
 
 function AllEvents() {

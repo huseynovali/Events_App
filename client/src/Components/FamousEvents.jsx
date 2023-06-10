fc4b587da9580b95e63b4164253ac0cc56d5b3ee
User <huseynovali612@gmail.com>
2023-06-11 00:16:04 +0400

    change main page

--- client/src/Components/FamousEvents.jsx
+++ client/src/Components/FamousEvents.jsx
@@ -47,57 +47,57 @@ const FamousEvents = () => {
   return (
     <div className="famous-events">
       <div className="button-container">
-        <Button className="prev-btn"  onClick={handlePrev}>
-            <ArrowBackIosNewIcon className="back-btn" />
-            <ArrowBackIosNewIcon className="back-btn" />
+        <Button className="prev-btn" onClick={handlePrev}>
+          <ArrowBackIosNewIcon className="back-btn" />
+          <ArrowBackIosNewIcon className="back-btn" />
 
-<ArrowBackIosNewIcon className="back-btn" />
+          <ArrowBackIosNewIcon className="back-btn" />
         </Button>
       </div>
 
       <Swiper
-  ref={swiperRef}
-  slidesPerView={3}
-  spaceBetween={100}
-  autoplay={{
-    delay: 2000,
-    disableOnInteraction: false,
-  }}
-  loop={true}
-  navigation={true}
-  pagination={{
-    clickable: true,
-  }}
-  className="mySwiper"
-  style={{ padding: '30px 0' }}
-  breakpoints={{
-    320: {
-      slidesPerView: 1,
-    },
-    992: {
-      slidesPerView: 2,
-    },
-  }}
->
+        ref={swiperRef}
+        slidesPerView={3}
+        spaceBetween={100}
+        autoplay={{
+          delay: 2000,
+          disableOnInteraction: false,
+        }}
+        loop={true}
+        navigation={true}
+        pagination={{
+          clickable: true,
+        }}
+        className="mySwiper"
+        style={{ padding: '30px 0' }}
+        breakpoints={{
+          320: {
+            slidesPerView: 1,
+          },
+          992: {
+            slidesPerView: 2,
+          },
+        }}
+      >
         {tickets.map((item, index) => (
           <SwiperSlide key={index} className={`card`}>
-          <div className={`card-container`} key={index} style={{backgroundImage: `url(${item.image})`}}>
-            <div className='card-description'>
-              <h1>{item.title}</h1>
-              <p> {item.authors}</p>
-              <h3>{item.price}$</h3>
+            <div className={`card-container`} key={index} style={{ backgroundImage: `url(${item.image})` }}>
+              <div className='card-description'>
+                <h1>{item.title}</h1>
+                <p> {item.authors}</p>
+                <h3>{item.price}$</h3>
+              </div>
+
+              <Link className='check-out' to={`/${item.id}`}>{item.price}$ </Link>
+
             </div>
-            
-            <Link className='check-out' to={`/${item.id}`}>{item.price}$ </Link>
-            
-          </div>
           </SwiperSlide>
         ))}
       </Swiper>
 
       <div>
-        <Button className="next-btn"  onClick={handleNext}>
-          <ArrowForwardIosIcon className="forward-btn"/>
+        <Button className="next-btn" onClick={handleNext}>
+          <ArrowForwardIosIcon className="forward-btn" />
           <ArrowForwardIosIcon className="forward-btn" />
 
           <ArrowForwardIosIcon className="forward-btn" />
